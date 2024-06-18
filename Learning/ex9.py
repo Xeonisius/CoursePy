2b98c56f1958efbd05a2f78ea165ff152f0c194b
neromelt <stanislav.bulatskii@gmail.com>
2024-06-18 20:26:44 +0200

    new ex

---
+++ Learning/ex9.py
@@ -0,0 +1,21 @@
+class multifilter:
+    def judge_half(pos, neg):
+        return pos >= neg
+
+    def judge_any(pos, neg):
+        return pos >= 1
+
+    def judge_all(pos, neg):
+        return neg == 0
+
+    def __init__(self, iterable, *funcs, judge=judge_any):
+        self.iterable = iterable
+        self.funcs = funcs
+        self.judge = judge
+
+    def __iter__(self):
+        for item in self.iterable:
+            pos = sum(f(item) for f in self.funcs)
+            neg = len(self.funcs) - pos
+            if self.judge(pos, neg):
+                yield item
